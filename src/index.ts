b6eb3be86954f59e0c6bb5ff4ba0505967dbf59a
shimura.atsushi <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2024-12-15 21:59:05 +0900

    some update

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ import * as fs from 'fs';
   const res = await getList("markdownai");
   const json = JSON.stringify(res);
   console.log(json);
-  fs.writeFileSync('./dist/contexts.json', json);
+  fs.writeFileSync('./dist/contents.json', json);
 })();
 
